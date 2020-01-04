4c125502601c1db4db3b4e72983e1d849fae7ee4
Patrick <pomara16@gmail.com>
2020-01-03 19:39:51 -0800

    second commit

--- script.js/script.js
+++ script.js/script.js
@@ -3,19 +3,20 @@ var groupSize= prompt ("How many people are in your group?");
 var result;
 
 if (vacationType=="musical"){
-    var destination="I reccomend New Orleans, Don't forget the deignets!";
+    var destination="I reccomend New Orleans, Don't forget the beignets!";
 } else if (vacationType=="tropical"){
     var destination="I reccomend a beach vacation, in Mexico, bring shorts and sandals";
 } else if (vacationType=="adventurous"){
     var destination="You'll love white water rafting the Grand Canyon, bring water proof everything";
 }
-var groupSize= prompt ("How many people are in your party?");
 if (groupSize<3){
-    var travel="You should travel first class, Cheers!";
-    }else if (groupSize >4<6){
-        var travel="You Should travel by helecopter, bring ear plugs";
-    }else if (groupSize >6){
-        var travel="You should travel via Charter FLight, don't forget your tablet!";
+    var travel= "You should travel first class, Cheers!";
+    }
+    else if (groupSize>=3&&groupSize<=5){
+        var travel= "You Should travel by helecopter, bring ear plugs";
+    }
+    else if (groupSize>5){
+        var travel= "You should travel via Charter Flight, don't forget your tablet!";
     }
     var result=(destination)+(travel);
     console.log(result)
